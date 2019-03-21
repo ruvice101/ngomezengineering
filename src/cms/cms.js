82e731a8c8673c95782c598c7461677d54658915
Kami Ruvice - Lenovo <kamiruvice@gmail.com>
2019-03-21 12:08:31 +0100

    Removed blog post and added TrainingDetailPreview to cms.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,11 @@
 import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+import TrainingDetailPreview from './preview-templates/TrainingDetailPreview';
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('training', TrainingDetailPreview)
--- src/cms/preview-templates/TrainingDetailPreview.js
+++ src/cms/preview-templates/TrainingDetailPreview.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { BlogPostTemplate } from '../../templates/blog-post'
+import { TrainingDetailTemplate } from '../../templates/training-detail';
 
-const BlogPostPreview = ({ entry, widgetFor }) => (
-  <BlogPostTemplate
+const TrainingDetailPreview = ({ entry, widgetFor }) => (
+  <TrainingDetailTemplate
     content={widgetFor('body')}
     description={entry.getIn(['data', 'description'])}
     tags={entry.getIn(['data', 'tags'])}
@@ -11,11 +11,11 @@ const BlogPostPreview = ({ entry, widgetFor }) => (
   />
 )
 
-BlogPostPreview.propTypes = {
+TrainingDetailPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
 }
 
-export default BlogPostPreview
+export default TrainingDetailPreview
